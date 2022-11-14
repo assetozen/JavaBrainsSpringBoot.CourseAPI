1da1b53c1551c7aafbe3b459ecec92258bf36b75
assetozen <assetozen@gmail.com>
2022-11-14 11:07:43 +0100

    updated post put delete methods are added

--- src/main/java/io/javabrains/springbootstarter/topic/TopicService.java
+++ src/main/java/io/javabrains/springbootstarter/topic/TopicService.java
@@ -2,19 +2,40 @@ package io.javabrains.springbootstarter.topic;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class TopicService {
-    List<Topic> topics = Arrays.asList(
+    List<Topic> topics = new ArrayList<>(Arrays.asList(
             new Topic("spring", "Spring Framework", "Spring Framework Description"),
             new Topic("java", "Core Java", "Core Java Description"),
             new Topic("javascript", "Javascript", "Javascript Description")
-    );
+    ));
 
-    public List<Topic> getAllTopics()
-    {
+    public List<Topic> getAllTopics() {
         return topics;
+    }
+
+    public Topic getTopic(String id) {
+        return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+    }
+
+    public void addTopic(Topic topic) {
+        topics.add(topic);
+    }
+
+    public void updateTopic(Topic topic, String id) {
+        for (Topic t : topics) {
+            if (t.getId().equals(id)) {
+                topics.set(topics.indexOf(t), topic);
+                return;
+            }
+        }
+    }
+
+    public void deleteTopic(String id) {
+        topics.removeIf(t -> t.getId().equals(id));
     }
 }
